fc22dd1bd13f4a539ee9492b943391856c874039
StatelyCloud <Release Bot infra@stately.cloud>
2023-12-11 04:25:24 +0000

    Update from https://github.com/StatelyCloud/stately/commit/4c3b506a33f61e8ca3d2fc309ffaa475f6875e92

--- version/version_test.go
+++ version/version_test.go
@@ -11,5 +11,11 @@ import (
 
 func TestGetVersion(t *testing.T) {
 	// regex for semver from here: https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
-	assert.Regexp(t, regexp.MustCompile(`^v(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$`), version.GetVersion())
+	assert.Regexp(
+		t,
+		regexp.MustCompile(
+			`^v(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$`,
+		),
+		version.GetVersion(),
+	)
 }
